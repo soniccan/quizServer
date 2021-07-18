37156eb9d25deae65f66d71e8bd330d8c61d8017
Baba-Syunsuke <babababa0415@icloud.com>
2021-07-18 19:38:11 +0900

    dev/null

--- quizserver/MulticlientServer.java
+++ quizserver/MulticlientServer.java
@@ -1,43 +1,71 @@
 package quizserver;
 import java.net.Socket;
 import java.net.ServerSocket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.IOException;
-
-public class MulticlientServer{
-    public static final int PORT =1234;
-
-    public static void main(String args[])
-    {
-        ServerSocket serverSocket = null;
-        Socket socket = null;
-        try {
-            serverSocket = new ServerSocket(PORT);
-            System.out.println("Server up port="+ serverSocket.getLocalPort());
-            while(true)
-            {
-                socket = serverSocket.accept();
-                new ServerThread(socket).run();
-            }
-
-        }catch (IOException e)
-        {
-            e.printStackTrace();
-        }finally
-        {
-            try{
-                if(socket != null)
-                {
-                    socket.close();
-                }
-            }catch (IOException e){}
-            try {
-                if(serverSocket != null)
-                {
-                    serverSocket.close();
-                }
-            } catch (IOException e){}
+  
+ public class MulticlientServer {
+  
+  public static final int ECHO_PORT = 1234;
+ 
+  public static void main(String args[]) {
+    ServerSocket serverSocket = null;
+    try {
+      serverSocket = new ServerSocket(ECHO_PORT);
+      System.out.println("EchoServerが起動しました(port="
+                         + serverSocket.getLocalPort() + ")");
+      while (true) {
+        Socket socket = serverSocket.accept();
+        new EchoThread(socket).start();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (serverSocket != null) {
+          serverSocket.close();
         }
+      } catch (IOException e) {}
     }
-    
-
+  }
+ 
+}
+ 
+class EchoThread extends Thread {
+ 
+  private Socket socket;
+ 
+  public EchoThread(Socket socket) {
+    this.socket = socket;
+    System.out.println("接続されました "
+                       + socket.getRemoteSocketAddress());
+  }
+ 
+  public void run() {
+    try {
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      String line;
+      while ( (line = in.readLine()) != null ) {
+        System.out.println(socket.getRemoteSocketAddress()
+                           + " 受信: " + line);
+        out.println(line);
+        System.out.println(socket.getRemoteSocketAddress()
+                           + " 送信: " + line);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (socket != null) {
+          socket.close();
+        }
+      } catch (IOException e) {}
+      System.out.println("切断されました "
+                         + socket.getRemoteSocketAddress());
+    }
+  }
+ 
 }
