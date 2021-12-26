aa51abc5b8f58858c83fc8c341511a997e79ee26
Baba Shunsuke <soniccan8560@gmail.com>
2021-12-26 16:42:00 +0900

    12/26

--- new_server/NonblockingServer.java
+++ new_server/NonblockingServer.java
@@ -98,59 +98,61 @@
 
 package new_server;
 import java.net.InetSocketAddress;
- import java.nio.ByteBuffer;
- import java.nio.channels.DatagramChannel;
- import java.nio.channels.SelectionKey;
- import java.nio.channels.Selector;
- import java.nio.charset.Charset;
- import java.util.Iterator;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.charset.Charset;
+import java.util.Iterator;
 
 
- public class NonblockingServer {
-     public static void main(String[] args) {
-         DatagramChannel datagramChannel = null;
-         try {
-             datagramChannel = DatagramChannel.open();
-             InetSocketAddress inetSocketAddress = new InetSocketAddress(10007);
-             System.out.println(inetSocketAddress.getHostName() + " " + inetSocketAddress.getPort() +"Open");
-             datagramChannel.socket().bind(inetSocketAddress);
-             datagramChannel.configureBlocking(false);
-             Selector selector = Selector.open();
-             datagramChannel.register(selector, SelectionKey.OP_READ);
-             while (selector.select() > 0) {
-                 for (Iterator iterator = selector.selectedKeys().iterator();iterator.hasNext();) {
-                     
-                    SelectionKey selectionKey = (SelectionKey)iterator.next();
-                    iterator.remove();
-                    if (selectionKey.isReadable()) {
-                        doReceive(selectionKey);
-                    }
+public class NonblockingServer {
+    public static void main(String[] args) {
+        DatagramChannel datagramChannel = null;
+        try {
+            datagramChannel = DatagramChannel.open();
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(10007);
+            System.out.println(inetSocketAddress.getHostName() + " " + inetSocketAddress.getPort() +"Open");
+            datagramChannel.socket().bind(inetSocketAddress);
+            
+            datagramChannel.configureBlocking(false);
+            Selector selector = Selector.open();
+            datagramChannel.register(selector, SelectionKey.OP_READ);
+
+            while (selector.select() > 0) {
+                for (Iterator iterator = selector.selectedKeys().iterator();iterator.hasNext();) {
                     
-                 }
-             }
-         } catch (Exception e) {
-             e.printStackTrace();
-         } finally {
-             try {
-                 datagramChannel.close();
-             } catch (Exception e) {}
-         }
+                SelectionKey selectionKey = (SelectionKey)iterator.next();
+                iterator.remove();
+                if (selectionKey.isReadable()) {
+                    doReceive(selectionKey);
+                }
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                datagramChannel.close();
+            } catch (Exception e) {}
+        }
 
 
-     }
+    }
 
 
-     private static void doReceive(SelectionKey selectionKey) {
-         DatagramChannel datagramChannel 
-                     = (DatagramChannel) selectionKey.channel();
-         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-         Charset charset = Charset.forName("Shift-JIS");
-         try {
-             datagramChannel.receive(byteBuffer);
-             byteBuffer.flip();
-             System.out.println(charset.decode(byteBuffer));
-         } catch (Exception e) {
-             e.printStackTrace();
-         }
-     }
- }
+    private static void doReceive(SelectionKey selectionKey) {
+        DatagramChannel datagramChannel 
+                    = (DatagramChannel) selectionKey.channel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        Charset charset = Charset.forName("Shift-JIS");
+        try {
+            datagramChannel.receive(byteBuffer);
+            byteBuffer.flip();
+            System.out.println(charset.decode(byteBuffer));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
