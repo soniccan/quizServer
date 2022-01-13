100591917b4e755df5ef60af0e3a8c7a68155027
Baba Shunsuke <soniccan8560@gmail.com>
2022-01-13 16:36:13 +0900

    1/13

--- quizserver/ServerData.java
+++ quizserver/ServerData.java
@@ -11,14 +11,14 @@ public class ServerData {
 
     // リスト(Map)を作る。ipaddressをキーとして、現在のクイズ正答数|正しいを答えを返せるMap
     Map<String, Integer> map;
-    String quizstatement[]  ={"1+1","2+2","3+3"};
+    String quizStatement[]  ={"1+1","2+2","3+3"};
     String ans[]  ={"2","4","6"};
 
     public ServerData(){
         this.map = new HashMap<>();
     }
-    
-    
+
+
 
     public String Parse(String line, ServerData server_date) {
 
@@ -26,30 +26,30 @@ public class ServerData {
         String message = line.substring(37,line.length());
         Integer quiz_number_now;
         String parsedMessage ="";
-        
-        
+
+
 
         if( (quiz_number_now = this.map.get(id)) == null  )
-        {  
+        {
             this.map.put(id,0);
             quiz_number_now =0;
             parsedMessage = "You (" + message + ") join game\n";
         }
         else if (isCollectAnswer(message,quiz_number_now))
-        {  
+        {
 
             quiz_number_now = this.map.get(id);
             this.map.put(id,++quiz_number_now);
 
-            parsedMessage = "You anwer is collect next quiz number is (" + quiz_number_now + ")  \n"; 
+            parsedMessage = "You answer is collect next quiz number is (" + quiz_number_now + ")  \n";
         }
         else {
-            parsedMessage = "You anwer is not collect next quiz number is (" + quiz_number_now + ")  \n"; 
+            parsedMessage = "You answer is not collect next quiz number is (" + quiz_number_now + ")  \n";
         }
-        
 
-        parsedMessage = parsedMessage + quizstatement[quiz_number_now] + "\n";
-        
+
+        parsedMessage = parsedMessage + quizStatement[quiz_number_now] + "\n";
+
         return parsedMessage;
 
 
@@ -61,13 +61,13 @@ public class ServerData {
         return ans[quiz_number_now].equals(message);
     }
 
-    
 
 
 
 
 
-    
+
+
 
 
 }
